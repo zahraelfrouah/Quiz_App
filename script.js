3c48cab896b9a090c4b6cb52a88ab3087fe1433d
zahra elfrouah <116457214+zahraelfrouah@users.noreply.github.com>
2023-11-19 15:58:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+const quizData = [
+  {
+      question: "What is the correct JavaScript syntax to change the content of the HTML element below?",
+      
+      a: "document.getElementByName('p').innerHTML='Hello World!';",
+      b: "document.getElementById('demo').innerHTML='Hello World!';",
+      c: "#demo.innerHTML = 'Hello World!';",
+      d: "document.getElement('p').innerHTML = 'Hello World!'",
+      correct: "b",
+  },
+  {
+      question: "How do you find the number with the highest value of x and y?",
+      a: "top(x, y) ",
+      b: "Math.max(x, y)",
+      c: "Math.ceil(x, y)",
+      d: "ceil(x, y)",
+      correct: "b",
+  },
+  {
+      question: "What does HTML stand for?",
+      a: "Hypertext Markup Language",
+      b: "Hypertext Markdown Language",
+      c: "Hyperloop Machine Language",
+      d: "Helicopters Terminals Motorboats Lamborginis",
+      correct: "a",
+  },
+  {
+      question: "What year was JavaScript launched?",
+      a: "1996",
+      b: "1995",
+      c: "1994",
+      d: "none of the above",
+      correct: "b",
+  },
+];
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+let currentQuiz = 0
+let score = 0
+loadQuiz()
+function loadQuiz() {
+  deselectAnswers()
+  const currentQuizData = quizData[currentQuiz]
+  questionEl.innerText = currentQuizData.question
+  a_text.innerText = currentQuizData.a
+  b_text.innerText = currentQuizData.b
+  c_text.innerText = currentQuizData.c
+  d_text.innerText = currentQuizData.d
+}
+function deselectAnswers() {
+  answerEls.forEach(answerEl => answerEl.checked = false)
+}
+function getSelected() {
+  let answer
+  answerEls.forEach(answerEl => {
+      if(answerEl.checked) {
+          answer = answerEl.id
+      }
+  })
+  return answer
+}
+submitBtn.addEventListener('click', () => {
+  const answer = getSelected()
+  if(answer) {
+     if(answer === quizData[currentQuiz].correct) {
+         score++
+     }
+     currentQuiz++
+     if(currentQuiz < quizData.length) {
+         loadQuiz()
+     } else {
+         quiz.innerHTML = `
+         <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+         <button onclick="location.reload()">Reload</button>
+         `
+     }
+  }
+})
